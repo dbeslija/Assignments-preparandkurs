99ae98c39b92959197e819171057bf5e8e38f1f2
dbeslija <dzemila.beslija@gmail.com>
2025-10-16 07:43:57 +0200

    First assignment

---
+++ Assignment 1 Dzemila Beslija/scripts/script.js
@@ -0,0 +1,9 @@
+function divide(){
+    let foo = 0;
+    let bar = 0;
+    let result = foo/bar;
+    if (isNaN(result)){
+        alert ("You cant divide us!")
+    }
+    console.log("button clicked",result )
+}
